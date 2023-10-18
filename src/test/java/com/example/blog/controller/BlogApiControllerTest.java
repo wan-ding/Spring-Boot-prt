d36d4bd93e461d2f9f77c4fa0ff996365a07aba0
wan-ding <weew9900@gmail.com>
2023-10-18 15:23:18 +0900

    [init] 테스트 코드 작성 #0

--- src/test/java/com/example/blog/controller/BlogApiControllerTest.java
+++ src/test/java/com/example/blog/controller/BlogApiControllerTest.java
@@ -1,5 +1,32 @@
 package com.example.blog.controller;
 
+import com.example.blog.repository.BlogRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 class BlogApiControllerTest {
 
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper; // 직렬화, 역직렬화를 위한 클래스
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @BeforeEach // 테스트 실행 전 실행하는 메서드
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+
+        blogRepository.deleteAll();
+    }
     }

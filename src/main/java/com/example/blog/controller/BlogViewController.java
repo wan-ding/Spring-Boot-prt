eaf4fcb44ce9ec8bd0fbd0359edad166008db98b
wan-ding <weew9900@gmail.com>
2023-10-20 10:02:41 +0900

    [feat] [컨트롤러] view 컨트롤러 작성

--- src/main/java/com/example/blog/controller/BlogViewController.java
+++ src/main/java/com/example/blog/controller/BlogViewController.java
@@ -1,4 +1,26 @@
 package com.example.blog.controller;
 
+import com.example.blog.dto.ArticleListViewResponse;
+import com.example.blog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
 public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new).toList();
+        model.addAttribute("articles", articles); // 블로그 글 리스트 저장
+
+        return "articleList"; //articleList.html라는 뷰 조회
+    }
 }

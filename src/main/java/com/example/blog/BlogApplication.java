d75f0763273b353f90d20cf4c3e7ef4804ff5bb6
wan-ding <weew9900@gmail.com>
2023-10-26 09:53:57 +0900

    [feat] [view] 글 보여주기 dto 등록

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -3,7 +3,9 @@ package com.example.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // created_at, updated_at 자동 업데이트
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BlogApplication {
 
--- src/main/java/com/example/blog/domain/Article.java
+++ src/main/java/com/example/blog/domain/Article.java
@@ -5,8 +5,12 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -23,6 +27,15 @@ public class Article {
 
     @Column(name = "content", nullable = false) // content라난 not null 컬럼과 매핑
     private String content;
+
+    @CreatedDate // 엔티티가 생성될 때 생성 시간 저장
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate //엔티티가 수정될 때 수정 시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
 
     @Builder
     public Article(String title, String content) {

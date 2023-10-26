ba5c940e2407764154d16478920705866b368069
wan-ding <weew9900@gmail.com>
2023-10-26 09:58:21 +0900

    [feat] [html] 블로그 글 보여주기 화면 만들기(article)

--- src/main/java/com/example/blog/controller/BlogViewController.java
+++ src/main/java/com/example/blog/controller/BlogViewController.java
@@ -1,11 +1,13 @@
 package com.example.blog.controller;
 
+import com.example.blog.domain.Article;
 import com.example.blog.dto.ArticleListViewResponse;
 import com.example.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +24,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles); // 블로그 글 리스트 저장
 
         return "articleList"; //articleList.html라는 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleListViewResponse(article));
+
+        return "article"; // article.html로 보내준다.
     }
 }
